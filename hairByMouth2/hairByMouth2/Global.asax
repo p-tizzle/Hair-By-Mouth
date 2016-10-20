471d26f8e5005ec14c9848eab3c34438779f36fc
jerry <jerrytinney@icloud.com>
2016-10-20 16:19:18 -0700

    started with FB login

---
+++ hairByMouth2/hairByMouth2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hairByMouth2.MvcApplication" Language="C#" %>
---
+++ hairByMouth2/hairByMouth2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hairByMouth2.Startup))]
+namespace hairByMouth2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
