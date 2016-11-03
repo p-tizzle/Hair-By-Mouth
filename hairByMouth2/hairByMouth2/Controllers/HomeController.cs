7d85da257d9a8028b37c9dc0bb3dd685293d7174
jerry <jerrytinney@icloud.com>
2016-11-03 16:36:36 -0700

    Added database to HairForm view

--- hairByMouth2/hairByMouth2/Controllers/HomeController.cs
+++ hairByMouth2/hairByMouth2/Controllers/HomeController.cs
@@ -1,18 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using hairByMouth2.Data;
 using hairByMouth2.Models;
 
 namespace hairByMouth2.Controllers
 {
     public class HomeController : Controller
     {
+        private HairContext db = new HairContext();
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult List()
+        {
+            return View(db.EntryModels.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EntryModel entryModel = db.EntryModels.Find(id);
+            if (entryModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entryModel);
+        }
+
+
+
+
 
         public ActionResult About()
         {
@@ -48,11 +77,100 @@ namespace hairByMouth2.Controllers
             return View("Hairform");
         }
 
-        [HttpPost]
-        public ActionResult HairEntry(EntryModel entrymodel)
+       
+        public ActionResult HairEntry()
         {
 
-            return View("HairForm");
+            return View();
+        }
+        // GET: EntryModels/Create
+
+
+        // POST: EntryModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HairEntry([Bind(Include = "ID,yourName,salonName,stylistName")] EntryModel entryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EntryModels.Add(entryModel);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(entryModel);
+        }
+
+    
+
+    // GET: EntryModels/Edit/5
+    public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EntryModel entryModel = db.EntryModels.Find(id);
+            if (entryModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entryModel);
+        }
+
+        // POST: EntryModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,yourName,salonName,stylistName")] EntryModel entryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(entryModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(entryModel);
+        }
+
+        // GET: EntryModels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EntryModel entryModel = db.EntryModels.Find(id);
+            if (entryModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entryModel);
+        }
+
+        // POST: EntryModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EntryModel entryModel = db.EntryModels.Find(id);
+            db.EntryModels.Remove(entryModel);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
-    }
+}
+
+
+        
